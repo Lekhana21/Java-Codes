72e8f2758206ab6eac1ed1a5bb9154f703aa02df
Lekhana21 <lekhana120702@gmail.com>
2025-09-07 16:40:11 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,21 @@
+package com.assn.go;
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+		
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			for(int k=1;k<=n-i;k++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i*2-1;j++) {
+		 		System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
